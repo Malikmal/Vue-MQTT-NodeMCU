d7073071af3e4641ac03b1bcefacf1394e216a10
ikmalil birri <malikmal98@gmail.com>
2020-10-27 01:04:39 +0700

    finishing"

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -79,7 +79,7 @@
           </li> -->
 
           <li class="flex items-center">
-            <button
+            <a
               class="bg-green-500 text-white active:bg-green-600 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
               type="button"
               href="https://github.com/Malikmal/Vue-IoT-MQTT"
@@ -87,7 +87,7 @@
               <!-- <i class="fas fa-arrow-alt-circle-down"></i>  -->
               <i class="text-white-500 fab fa-github text-lg leading-lg mr-2" />
               Source code
-            </button>
+            </a>
           </li>
         </ul>
       </div>
