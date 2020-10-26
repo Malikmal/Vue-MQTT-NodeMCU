9b770dbd57c1b6914afaaed3ed27fb72e54955b1
ikmalil birri <malikmal98@gmail.com>
2020-10-27 01:05:52 +0700

    finishing"

--- NodeMCU-Lolin-v3.ino
+++ NodeMCU-Lolin-v3.ino
@@ -1,45 +1,29 @@
-/*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
-*/
+/*******************************************************
+ * Kelompok 3 
+ * Workshop Kompilasi
+ * 4 D4 Teknik Komputer A
+ * Politeknik Elektronika Negeri Surabaya (EEPIS)
+ ********************************************************/
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
 
-// Update these with values suitable for your network.
 
+//mqtt configuration topic
+#define topicPotensio   "tekkoma/potensio/1"
+#define topicLed        "tekkoma/led"
+
+// Update these with values suitable for your network.
 const char* ssid = "lalalala";
 const char* password = "akudewelali";
-const char* mqtt_server = "test.mosquitto.org";
+const char* mqtt_server = "test.mosquitto.org";//"broker.mqtt-dashboard.com";//"test.mosquitto.org";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-long lastMsg = 0;
 char msg[50];
-int value = 0;
+int potensio = 0, a = 0;
 
 void setup_wifi() {
-
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -73,8 +57,6 @@ void callback(char* topic, byte* payload, unsigned int length) {
   // Switch on the LED if an 1 was received as first character
   if ((char)payload[0] == '1') {
     digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
   } else {
     digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
   }
@@ -86,23 +68,37 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
-    String clientId = "ESP8266Client-";
+    String clientId = "TekkomAClient-";
     clientId += String(random(0xffff), HEX);
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.publish("tekkoma/potensio/1", "0");
+      client.publish(topicLed, "0");
       // ... and resubscribe
-      client.subscribe("tekkoma/led");
+      client.subscribe(topicLed);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
-      delay(5000);
+      delay(2000);
     }
   }
+}
+
+void readAnalogPin() {
+   if( millis() % 50 != 0 ) // give 50 milliseconds  for potensio time
+       return;
+  a = analogRead(A0);
+  a = map(a, 0, 1023, 0, 255);
+  if(abs(potensio - a) >= 5 ) { //my potensio is suck so i filter it
+    sprintf (msg,  "%ld", potensio);
+    client.publish(topicPotensio, msg);
+    Serial.println(msg);
+    delay(50);
+  }
+  potensio = a;
 }
 
 void setup() {
@@ -114,20 +110,9 @@ void setup() {
 }
 
 void loop() {
-
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("tekkoma/potensio/1", msg);
-//    client.publish("test", "wkwkwk");
-  }
+  readAnalogPin();
 }
